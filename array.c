e7ef18a89b7a8016a4bbbc17ebdad4182a098b09
Naman Chachan <namanchachan@Namans-MacBook-Air.local>
2024-01-06 13:28:58 +0530

    Changes in Array code

--- array.c
+++ array.c
@@ -7,13 +7,13 @@ int marks[4];
 
 for(int i = 0; i < 4; i++)
 {
-    printf("enter the value of %d of the element of the array", i);
+    printf("Enter the value of %d of the element of the array", i);
     scanf("%d\n", &marks[i]);
 }
 
 for (int i = 0; i < 4; i++)
 {
-    printf("the value of %d element of tha array is %d\n", i, marks[i]);
+    printf("The value of %d element of tha array is %d\n", i, marks[i]);
 }
 
 return 0;
